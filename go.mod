f140147eca3de352c924823636c8f3c02931316c
Bayu Setiawan <bayusetiawanipb@gmail.com>
2026-04-14 16:35:24 +0700

    update go-say-hello dependency to v1.0.4 and refactor import statement

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/codebayu/account-service
 
 go 1.26.2
 
-require github.com/codebayu/go-say-hello v1.0.1 // indirect
+require github.com/codebayu/go-say-hello v1.0.4 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import ( 
 	"fmt" 
-	go_say_hello "github.com/codebayu/go-say-hello" 
+	sayhello "github.com/codebayu/go-say-hello" 
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello("Bayu"))	
+	   fmt.Println(sayhello.SayHello("bayu"))
 }
